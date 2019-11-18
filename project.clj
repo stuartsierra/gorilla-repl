d1fb0f179365bfbcff5a94bb62ebe66974da8f9b
Stuart Sierra <mail@stuartsierra.com>
2019-11-18 17:08:19 -0500

    Prep project.clj for my 0.7.0 release
    Using my own Group ID on Clojars, since this is a fork.
    See https://github.com/clojars/clojars-web/wiki/Tutorial

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
 ;;;;
 ;;;; gorilla-repl is licenced to you under the MIT licence. See the file LICENCE.txt for full details.
 
-(defproject org.clojars.stuartsierra/gorilla-repl "0.7.0-SNAPSHOT"
-  :description "A rich REPL for Clojure in the notebook style."
-  :url "https://github.com/benfb/gorilla-repl"
+(defproject org.clojars.stuartsierra/gorilla-repl "0.7.0"
+  :description "Custom fork of Gorilla REPL from benfb/gorilla-repl"
+  :url "https://github.com/stuartsierra/gorilla-repl"
   :license {:name "MIT"}
   :dependencies ^:replace [[org.clojure/clojure "1.10.1"]
                            [http-kit "2.3.0" :exclusions [ring/ring-core]]
@@ -25,9 +25,8 @@
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
-                  ["vcs" "tag" "v" "--no-sign"]
+                  ["vcs" "tag" "v"]
                   ["deploy"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
-                  ["vcs" "push"]]
-  :deploy-repositories {"releases" {:url "https://repo.clojars.org" :username :env :password :env :sign-releases false}})
+                  ["vcs" "push"]])
